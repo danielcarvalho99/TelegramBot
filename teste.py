dd13c828ca6fc45404aed1289d3c449d2cb6aa9a
danielcarvalho99 <danielcd3099@gmail.com>
2020-08-19 19:28:49 -0300

    deploy

---
+++ teste.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+import telepot
+from telepot.loop import MessageLoop
+from funcoes import encontrarPrimeiroParagrafo,acharSinopsedoArco,api
+
+bot = telepot.Bot(api)
+textoInicial = "Olá, este é o OnePieceWikiPtBot! Digite um número e receberá a sinopse do arco"
+
+def exibirDados(msg):
+    try:
+        print(msg['from']['first_name'] + " " + msg['from']['last_name'] )
+    except:
+        print(msg['from']['first_name'])
+    
+    print(msg['text'])
+
+def mandarLinkDoArco(msg):
+    _id = msg['from']['id']
+    bot.sendMessage(_id,"Oi")
+
+def receberMensagem(msg):
+    try:
+        mandarLinkDoArco(msg)
+    except:
+        pass
+
+
+
+bot.message_loop(receberMensagem)
+
+while True:
+    pass
